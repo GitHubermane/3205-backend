65d861c2e3b5326723d88bd4573676238cc45d67
Viktor Kryuchkov <crykour12@gmail.com>
2023-12-01 17:04:54 +0300

    add logger

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,21 @@
 import express from "express";
-import expressWinston from "express-winston";
-import { format, transports } from "winston";
+import { morganMiddleware } from "./middleware/morgan.js";
+import router from "./routes.js";
+import { logger } from "./utils/logger.js";
 
 const app = express();
 const PORT = process.env.PORT ?? 5100;
 
-app.use(
-  expressWinston.logger({
-    transports: [new transports.Console()],
-    format: format.combine(
-      format.json(),
-      format.timestamp(),
-      format.prettyPrint(),
-    ),
-  }),
-);
+app.use(morganMiddleware);
+app.use("/api", router);
 
 const start = async () => {
   try {
     app.listen(PORT, () => {
-      console.log(`Server has been started on port ${PORT}`);
+      logger.info(`Server has been started on port ${PORT}`);
     });
   } catch (e) {
-    console.log(e);
+    logger.error(e);
   }
 };
 
