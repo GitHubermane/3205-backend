1001b3ce2ddb0d596c998c1afc58829141f0a81e
Viktor Kryuchkov <crykour12@gmail.com>
2023-12-05 13:42:16 +0300

    add cors

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,18 @@ import express from "express";
 import { morganMiddleware } from "./middleware/morgan.js";
 import router from "./routes.js";
 import { logger } from "./utils/logger.js";
+import cors from "cors";
 import "dotenv/config";
 
 const app = express();
 const PORT = process.env.PORT ?? 5100;
 
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.API_URL,
+  }),
+);
 app.use(morganMiddleware);
 app.use("/api", router);
 
